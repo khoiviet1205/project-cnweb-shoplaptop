172e8a5540c51f56936c11b1bf748cfa251f87ff
khoiviet329@gmail.com <khoiviet329@gmail.com@d7faee81-5079-1b66-4d61-aba7667ffd92>
2013-10-31 17:06:43 +0000

    Sửa link đường dẫn file css (trong views/layout.php)

--- trunk/application/views/layout.php
+++ trunk/application/views/layout.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?php echo $title ?></title>
-<link rel="stylesheet" type="text/css" href="publics/css/style1.css"/>
+<link rel="stylesheet" type="text/css" href="publics/css/style.css"/>
 </head>
 <body>
 	<!--Begin banner-->
