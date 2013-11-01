46010c357a7e6ab573785a7519c75a584f6ef220
khoiviet329@gmail.com <khoiviet329@gmail.com@d7faee81-5079-1b66-4d61-aba7667ffd92>
2013-11-01 03:52:59 +0000

    V tách layout thành nhiều phần cho dễ quản lý

--- trunk/application/views/layout.php
+++ trunk/application/views/layout.php
@@ -3,93 +3,26 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?php echo $title ?></title>
-<link rel="stylesheet" type="text/css" href="publics/css/style.css"/>
+<link rel="stylesheet" type="text/css" href="publics/css/style1.css"/>
 </head>
 <body>
-	<!--Begin banner-->
-	<div id="banner">
-    	<div style="float:left;"><img src="publics/icon/Logo-lap.png" height="54" width="233" /></div>
-    	<div style="float:left; margin-left:180px;padding-top:10px;">
-            <object codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" 
-            classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" align="absmiddle" height="35" width="250">
-            <param value="250" name="width">
-            <param value="135" name="height">
-            <param value="high" name="quality">
-            <param value="transparent" name="wmode">
-            <param value="hangchinhhang.swf" name="src">
-            <embed src="publics/icon/hangchinhhang.swf" wmode="transparent" quality="high" type="application/x-shockwave-flash" height="35" width="250">
-        </object>
-     	<img src="publics/icon/hot-line.png" align="absmiddle" style="margin-left:70px;" />
-   	 	</div>
-    </div>
-    <!--End banner-->
-    
-    <!--Begin top menu-->
-    <div id="top_menu">
-	<ul>
-    	<li><a href="#">Trang Chủ</a></li>
-        <li><a href="#">Sản Phẩm</a></li>
-        <li><a href="#">Tin Tức</a></li>
-        <li><a href="#">Hỗ Trợ</a></li>
-        <li><a href="#">Mua Trả Góp</a></li>
-        <li><a href="#">Liên Hệ</a></li>
-    </ul>
-	</div>
-	<!--End top menu-->
-    
-    <div style="clear:left; height:39px;"></div>
-    
-    <!--Begin top search-->
-    <div id="top_search">
-	<span style="font-weight:bold;">Tìm kiếm :</span>
-    <select>
-    	<option>Chọn danh mục</option>
-    </select> 
-    <input type="text" name="txttext" />
-	<input type="submit" value="Search" />    
-	</div>
-    <!--End top search-->
-    
-    <!--Begin top đặt hàng-->
-    <div id="top_dathang">
-		<div class="left_dathang">
-		<img src="publics/icon/empty_cart_icon.gif" width="44" align="left" />&nbsp;<strong>Giỏ hàng rỗng</strong> </div>
-    	<div class="cen_dathang">
-        <img src="publics/icon/top_tools_delim.gif" align="left" style="margin-right:20px;" />Đặt Hàng</div>
-    	<div class="right_dathang"></div>
-	</div>
-    <!--End top đặt hàng-->
+	<!--Begin top-->
+		<?php $this->load->view("top")?>
     
     <!--Begin content-->
     <div id="content">
 	<div class="left_content">
-    	Menu trai o day
+    	<?php $this->load->view("left") ?>
     </div>
     <div class="main_content">content</div>
-    <div class="right_content">menu right</div>
+    <div class="right_content">
+    	<?php $this->load->view("right")?>
+    </div>
 	</div>
-	<!--End content-->
     
     <!--Begin bottom-->
-    <div id="bottom" align="center">
-	<span style="font-weight:bold; color:#FFFFFF">Tìm kiếm :</span>
-    <select>
-    	<option>Chọn danh mục</option>
-    </select> 
-    <input type="text" name="txttext" />
-	<input type="submit" value="Search" />    
-	</div>
-    <!--End bottom-->
+    	<?php $this->load->view("bottom") ?>
     
-    <!--Begin footter-->
-    <div id="footter" align="center">
-	<ul>
-    	<li><strong>TRƯỜNG ĐẠI HỌC CÔNG NGHỆ THÔNG TIN</strong></li>
-		<li>CÔNG NGHỆ WEB VÀ ỨNG DỤNG</li>
-        <li>NHÓM 2</li>
-     </ul>
-	</div>
-    <!--End footter-->
     
 </body>
 </html>
