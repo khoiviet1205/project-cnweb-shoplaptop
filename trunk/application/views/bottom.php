83ff58f47b9f483e267bb52faa6a83a185a09a86
khoiviet329@gmail.com <khoiviet329@gmail.com@d7faee81-5079-1b66-4d61-aba7667ffd92>
2013-11-01 04:06:00 +0000

    Update bottom.php

---
+++ trunk/application/views/bottom.php
@@ -0,0 +1,20 @@
+<div style="height:10px;"></div>
+</div>
+<div id="bottom" align="center">
+	<span style="font-weight:bold; color:#FFFFFF">Tìm kiếm :</span>
+    <select>
+    	<option>Chọn danh mục</option>
+    </select> 
+    <input type="text" name="txttext" />
+	<input type="submit" value="Search" />    
+</div>
+<div id="footter" align="center">
+	<ul>
+    	<li><strong>TRƯỜNG ĐẠI HỌC CÔNG NGHỆ THÔNG TIN</strong></li>
+		<li>CÔNG NGHỆ WEB VÀ ỨNG DỤNG</li>
+        <li>NHÓM 2</li>
+     </ul>
+</div>
+
+</body>
+</html>
