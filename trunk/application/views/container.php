738ac20aded45580aad715bd0d4eb53dafdc4569
ngocduong2310@gmail.com <ngocduong2310@gmail.com@d7faee81-5079-1b66-4d61-aba7667ffd92>
2013-11-03 15:42:04 +0000

    [Ngoc Dương][Thêm sản phẩm bán chạy]

--- trunk/application/views/container.php
+++ trunk/application/views/container.php
@@ -218,6 +218,88 @@
     </div>
   </section>
   
+  <!-- Selling Product-->
+  <section id="selling" class="row">
+    <div class="container">
+      <h1 class="heading1"><span class="maintext">Sản Phẩm Bán Chạy</span><span class="subtext"> Xem Tất Cả Sản Phẩm Mới Nhất</span></h1>
+      <ul class="thumbnails">
+        <li class="span3">
+          <a class="prdocutname" href="product.html">Lenovo G460</a>
+          <div class="thumbnail">
+            <a href="#"><img alt="" src="publics/img/product1a.jpg"></a>
+            <div class="shortlinks">
+              <a class="details" href="#">Chi Tiết</a>
+              <a class="wishlist" href="#">Ưa Thích</a>
+              <a class="compare" href="#">So Sánh</a>
+            </div>
+            <div class="pricetag">
+              <span class="spiral"></span><a href="#" class="productcart">Thêm Vào</a>
+              <div class="price">
+                <div class="pricenew">$4459.00</div>
+                <div class="priceold">$5000.00</div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="span3">
+          <a class="prdocutname" href="product.html">SamSung V1</a>
+          <div class="thumbnail">
+            <a href="#"><img alt="" src="publics/img/product2a.jpg"></a>
+            <div class="shortlinks">
+              <a class="details" href="#">Chi Tiết</a>
+              <a class="wishlist" href="#">Ưa Thích</a>
+              <a class="compare" href="#">So Sánh</a>
+            </div>
+            <div class="pricetag">
+              <span class="spiral"></span><a href="#" class="productcart">Thêm Vào</a>
+              <div class="price">
+                <div class="pricenew">$4459.00</div>
+                <div class="priceold">$5000.00</div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="span3">
+          <a class="prdocutname" href="product.html">Lenovo G560</a>
+          <div class="thumbnail">
+            <span class="new tooltip-test" >New</span>
+            <a href="#"><img alt="" src="publics/img/product1a.jpg"></a>
+            <div class="shortlinks">
+              <a class="details" href="#">Chi Tiết</a>
+              <a class="wishlist" href="#">Ưa Thích</a>
+              <a class="compare" href="#">So Sánh</a>
+            </div>
+            <div class="pricetag">
+              <span class="spiral"></span><a href="#" class="productcart">Thêm Vào</a>
+              <div class="price">
+                <div class="pricenew">$4459.00</div>
+                <div class="priceold">$5000.00</div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="span3">
+          <a class="prdocutname" href="product.html">ASUS</a>
+          <div class="thumbnail">
+            <a href="#"><img alt="" src="publics/img/product2a.jpg"></a>
+            <div class="shortlinks">
+              <a class="details" href="#">Chi Tiết</a>
+              <a class="wishlist" href="#">Ưa Thích</a>
+              <a class="compare" href="#">So Sánh</a>
+            </div>
+            <div class="pricetag">
+              <span class="spiral"></span><a href="#" class="productcart">Thêm Vào</a>
+              <div class="price">
+                <div class="pricenew">$4459.00</div>
+                <div class="priceold">$5000.00</div>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+  
   <!-- Section  Banner Start-->
   <section class="container smbanner">
     <div class="row">
