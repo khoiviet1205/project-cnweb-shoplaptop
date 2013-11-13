1013a539034a51a8326f097f9f33ff1d02cebfa7
khoiviet329@gmail.com <khoiviet329@gmail.com@d7faee81-5079-1b66-4d61-aba7667ffd92>
2013-11-13 11:46:41 +0000

    [Việt] Đưa nội dung, từ csdl lên layout cụ thể container.php:  <!-- Vip Product-->   <section id="latest" class="row">     <div class="container">       <h1 class="heading1"><span class="maintext">Sản Phẩm Cao Cấp</span><span class="subtext"> Xem Tất Cả Sản Phẩm Mới Nhất</span></h1>       <?php if(!empty($itemsspcc)) { ?>       <ul class="thumbnails">       	<?php foreach($itemsspcc as $item) : ?>         <li class="span3">           <a class="prdocutname" href="product.html"><?php echo $item['name_sp']; ?></a>           <div class="thumbnail">             <a href="#"><img alt="" src="<?php echo $item['img_sp']; ?>"></a>             <div class="shortlinks">               <a class="details" href="#">Chi Tiết</a>               <a class="wishlist" href="#">Ưa Thích</a>               <a class="compare" href="#">So Sánh</a>             </div>             <div class="pricetag">               <span class="spiral"></span><a href="#" class="productcart">Thêm Vào</a>               <div class="price">                 <div class="pricenew"><?php echo number_format(($item['price']-(($item['price']*$item['price_km'])/100)),0,',','.');?> đ</div>                 <div class="priceold"><?php echo number_format($item['price'],0,',','.'); ?></div>               </div>             </div>           </div>         </li>         <?php endforeach ?>       </ul>        <?php }else{ ?> 		Đang cập nhật... 		<?php } ?>     </div>   </section>

--- trunk/application/views/container.php
+++ trunk/application/views/container.php
@@ -222,6 +222,72 @@
   <section id="selling" class="row">
     <div class="container">
       <h1 class="heading1"><span class="maintext">Sản Phẩm Bán Chạy</span><span class="subtext"> Xem Tất Cả Sản Phẩm Mới Nhất</span></h1>
+      <?php if(!empty($items)) { ?>       	
+      <ul class="thumbnails">
+      	<?php foreach($items as $item) : ?>
+        <li class="span3">   
+          <a class="prdocutname" href="product.html"><?php echo $item['name_sp']; ?></a>
+          <div class="thumbnail">
+            <a href="#"><img alt="" src="<?php echo $item['img_sp']; ?>"></a>
+            <div class="shortlinks">
+              <a class="details" href="#">Chi Tiết</a>
+              <a class="wishlist" href="#">Ưa Thích</a>
+              <a class="compare" href="#">So Sánh</a>
+            </div>
+            <div class="pricetag">
+              <span class="spiral"></span><a href="#" class="productcart">Thêm Vào</a>
+              <div class="price">
+                <div class="pricenew"><?php echo number_format(($item['price']-(($item['price']*$item['price_km'])/100)),0,',','.');?> đ</div>
+                <div class="priceold"><?php echo number_format($item['price'],0,',','.'); ?> đ</div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <?php endforeach ?>
+      </ul>  
+        <?php }else{ ?>
+		Đang cập nhật...
+		<?php } ?>
+    </div>   
+  </section>
+  
+   <!-- Vip Product-->
+  <section id="latest" class="row">
+    <div class="container">
+      <h1 class="heading1"><span class="maintext">Sản Phẩm Cao Cấp</span><span class="subtext"> Xem Tất Cả Sản Phẩm Mới Nhất</span></h1>
+      <?php if(!empty($itemsspcc)) { ?>     
+      <ul class="thumbnails">
+      	<?php foreach($itemsspcc as $item) : ?>
+        <li class="span3">
+          <a class="prdocutname" href="product.html"><?php echo $item['name_sp']; ?></a>
+          <div class="thumbnail">
+            <a href="#"><img alt="" src="<?php echo $item['img_sp']; ?>"></a>
+            <div class="shortlinks">
+              <a class="details" href="#">Chi Tiết</a>
+              <a class="wishlist" href="#">Ưa Thích</a>
+              <a class="compare" href="#">So Sánh</a>
+            </div>
+            <div class="pricetag">
+              <span class="spiral"></span><a href="#" class="productcart">Thêm Vào</a>
+              <div class="price">
+                <div class="pricenew"><?php echo number_format(($item['price']-(($item['price']*$item['price_km'])/100)),0,',','.');?> đ</div>
+                <div class="priceold"><?php echo number_format($item['price'],0,',','.'); ?></div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <?php endforeach ?>
+      </ul>
+       <?php }else{ ?>
+		Đang cập nhật...
+		<?php } ?>
+    </div>
+  </section>
+
+ <!-- General Product-->
+  <section id="latest" class="row">
+    <div class="container">
+      <h1 class="heading1"><span class="maintext">Sản Phẩm Phổ Thông</span><span class="subtext"> Xem Tất Cả Sản Phẩm Mới Nhất</span></h1>
       <ul class="thumbnails">
         <li class="span3">
           <a class="prdocutname" href="product.html">Lenovo G460</a>
@@ -241,47 +307,19 @@
             </div>
           </div>
         </li>
+      </ul>
+    </div>
+  </section>
+  
+  <!-- Medium Product-->
+  <section id="latest" class="row">
+    <div class="container">
+      <h1 class="heading1"><span class="maintext">Sản Phẩm Trung Cấp</span><span class="subtext"> Xem Tất Cả Sản Phẩm Mới Nhất</span></h1>
+      <ul class="thumbnails">
         <li class="span3">
-          <a class="prdocutname" href="product.html">SamSung V1</a>
+          <a class="prdocutname" href="product.html">Lenovo G460</a>
           <div class="thumbnail">
-            <a href="#"><img alt="" src="publics/img/product2a.jpg"></a>
-            <div class="shortlinks">
-              <a class="details" href="#">Chi Tiết</a>
-              <a class="wishlist" href="#">Ưa Thích</a>
-              <a class="compare" href="#">So Sánh</a>
-            </div>
-            <div class="pricetag">
-              <span class="spiral"></span><a href="#" class="productcart">Thêm Vào</a>
-              <div class="price">
-                <div class="pricenew">$4459.00</div>
-                <div class="priceold">$5000.00</div>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="span3">
-          <a class="prdocutname" href="product.html">Lenovo G560</a>
-          <div class="thumbnail">
-            <span class="new tooltip-test" >New</span>
             <a href="#"><img alt="" src="publics/img/product1a.jpg"></a>
-            <div class="shortlinks">
-              <a class="details" href="#">Chi Tiết</a>
-              <a class="wishlist" href="#">Ưa Thích</a>
-              <a class="compare" href="#">So Sánh</a>
-            </div>
-            <div class="pricetag">
-              <span class="spiral"></span><a href="#" class="productcart">Thêm Vào</a>
-              <div class="price">
-                <div class="pricenew">$4459.00</div>
-                <div class="priceold">$5000.00</div>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="span3">
-          <a class="prdocutname" href="product.html">ASUS</a>
-          <div class="thumbnail">
-            <a href="#"><img alt="" src="publics/img/product2a.jpg"></a>
             <div class="shortlinks">
               <a class="details" href="#">Chi Tiết</a>
               <a class="wishlist" href="#">Ưa Thích</a>
@@ -299,7 +337,6 @@
       </ul>
     </div>
   </section>
-  
   <!-- Section  Banner Start-->
   <section class="container smbanner">
     <div class="row">
