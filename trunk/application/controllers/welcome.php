41141ba8f64f5bd62f66415c63a5bc0d17c6ce3f
ngocduong2310@gmail.com <ngocduong2310@gmail.com@d7faee81-5079-1b66-4d61-aba7667ffd92>
2013-11-13 11:35:08 +0000

    [Ngọc Dương] Gọi đến table product trong csdl từ class Mbase

--- trunk/application/controllers/welcome.php
+++ trunk/application/controllers/welcome.php
@@ -1,11 +1,34 @@
-<?php
-class Welcome extends CI_Controller{
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once APPPATH.'controllers/cpanel.php';
+
+class Welcome extends Cpanel{
 	public function __construct(){
 		parent ::__construct();
+		$this->_data['k']="";
+		$this->load->library('pagination');
 		$this->load->helper("url");
 	}
 	public function index(){
-		$data['title']="Shop Laptop";
-		$this->load->view("layout",$data);
+		//$this->_data['idCat']=NULL;
+		$arraySearch = array();
+		$column = "id_sp";
+		$orderBy = "asc";
+		$this->_data['title']="Shop Laptop";
+		$this->_data['template']='../views/container.php';
+		//$data['title']="Shop Laptop";
+		// Cấu hình phân trang
+		
+		$config['total_rows']=$this->Mproduct->countAll();
+		$number = $config['per_page']=8;
+		$config['uri_segment']=2;
+		$offset = (int) $this->uri->segment(2);
+		$this->pagination->initialize($config);
+		//$this->_data['data']['items'] = $this->Mproduct->getAll();
+		$this->_data['data']['items'] = $this->Mproduct->getAllPagingWhere($number,$offset,$arraySearch,$column,$orderBy);
+		$this->_data['data']['itemsspcc']=$this->Mbase->getspcc();
+		
+		$this->load->view("layout",$this->_data);
 	}
+
+	
 }
