e5b987526b034e1c6c4241d1dbaa42852b7e39e0
khoiviet329@gmail.com <khoiviet329@gmail.com@d7faee81-5079-1b66-4d61-aba7667ffd92>
2013-11-29 13:58:02 +0000

    [Việt]Sắp xếp lại code chỗ dễ nhìn

--- trunk/application/controllers/welcome.php
+++ trunk/application/controllers/welcome.php
@@ -4,31 +4,28 @@ require_once APPPATH.'controllers/cpanel.php';
 class Welcome extends Cpanel{
 	public function __construct(){
 		parent ::__construct();
-		$this->_data['k']="";
+		$this->data['k']="";
 		$this->load->library('pagination');
 		$this->load->helper("url");
 	}
 	public function index(){
+		$this->layout();
+	}
+	function layout(){
 		//$this->_data['idCat']=NULL;
 		$arraySearch = array();
 		$column = "id_sp";
 		$orderBy = "asc";
-		$this->_data['title']="Shop Laptop";
-		$this->_data['template']='../views/container.php';
+		$this->data['title']="Shop Laptop";
+		//$this->_data['template']='../views/container.php';
 		//$data['title']="Shop Laptop";
-		// Cấu hình phân trang
-		
-		$config['total_rows']=$this->Mproduct->countAll();
-		$number = $config['per_page']=8;
-		$config['uri_segment']=2;
-		$offset = (int) $this->uri->segment(2);
-		$this->pagination->initialize($config);
-		//$this->_data['data']['items'] = $this->Mproduct->getAll();
-		$this->_data['data']['items'] = $this->Mproduct->getAllPagingWhere($number,$offset,$arraySearch,$column,$orderBy);
-		$this->_data['data']['itemsspcc']=$this->Mbase->getspcc();
+		$this->data['itemsspcc']=$this->Mbase->getspcc();
 		$this->_data['data']['itemssppb']=$this->Mbase->getsppb();
-		$this->load->view("layout",$this->_data);
+		$this->_data['data']['itemssptc']=$this->Mbase->getsptc();
+		$this->load->view("layout",$this->data);
 	}
-
+	public function chitietsanpham(){
+		
+	}
 	
 }
