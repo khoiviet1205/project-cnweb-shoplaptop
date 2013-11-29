a04a760bf41c5d2c366b1c6185029e077e628bd9
khoiviet329@gmail.com <khoiviet329@gmail.com@d7faee81-5079-1b66-4d61-aba7667ffd92>
2013-11-29 13:50:31 +0000

    [Việt]Config chỗ autoload

--- trunk/application/config/config.php
+++ trunk/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/project_ci';
+$config['base_url']	= 'http://localhost/project_cnweb';
 
 /*
 |--------------------------------------------------------------------------
