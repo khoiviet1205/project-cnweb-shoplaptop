9d9c478d96ac540067a9724c43873d6c57449f8b
ngocduong2310@gmail.com <ngocduong2310@gmail.com@d7faee81-5079-1b66-4d61-aba7667ffd92>
2013-12-07 16:06:00 +0000

    [Dương]Test thử routes

--- trunk/application/config/routes.php
+++ trunk/application/config/routes.php
@@ -40,7 +40,7 @@
 
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
-
+$route['tatcasanpham/(:any)'] = "welcome/tatcasanpham/$1";
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
