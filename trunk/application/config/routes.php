44ea4b1805b00b33eef792183ce4fb4f2999df01
khoiviet329@gmail.com <khoiviet329@gmail.com@d7faee81-5079-1b66-4d61-aba7667ffd92>
2013-12-14 11:11:33 +0000

    [Việt]Sửa lỗi giỏ hàng

--- trunk/application/config/routes.php
+++ trunk/application/config/routes.php
@@ -85,9 +85,7 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 |
 */
 
-$route['default_controller'] = "welcome";
-$route['404_override'] = '';
-$route['tatcasanpham/(:any)'] = "welcome/tatcasanpham/$1";
+
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
