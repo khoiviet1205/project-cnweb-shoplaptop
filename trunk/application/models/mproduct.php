6a0545d9056efa740f5e3205adb9778b8c41a45f
khoiviet329@gmail.com <khoiviet329@gmail.com@d7faee81-5079-1b66-4d61-aba7667ffd92>
2013-12-15 04:53:34 +0000

    Backup

--- trunk/application/models/mproduct.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Mproduct extends Mbase{
-	
-	public function __construct(){
-		parent::__construct();
-		$this->setTableName("product");
-	}
-	
-}
--- trunk/application/views/container.php
+++
@@ -1,7 +0,0 @@
-<?php $this->load->view("sanphammoinhat",$itemsspm) ?>
-  
-<?php $this->load->view("sanphamcaocap",$itemsspcc)?>
-  
-<?php $this->load->view("sanphamphothong",$itemssppt)?> 
-  
-<?php $this->load->view("sanphamtrungcap",$itemssptc)?>
